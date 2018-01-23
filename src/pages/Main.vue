a2b84d4830cfbe019ff2e457ba926c610b152f4c
busyhe <525118368@qq.com>
2018-01-23 17:23:06 +0800

    Initial commit

---
+++ src/pages/Main.vue
@@ -0,0 +1,26 @@
+/**
+* Created by busyhe on 2018/1/23 下午4:59.
+* Email: 525118368@qq.com
+*/
+
+<template>
+    <div class="main">
+    </div>
+</template>
+<script>
+    export default {
+        name: 'Main',
+        data() {
+            return {}
+        },
+        props: [''],
+        components: {},
+        created() {
+        },
+        mounted() {
+        },
+        methods: {}
+    }
+</script>
+<style lang="stylus" rel="stylesheet/stylus" type="text/stylus">
+</style>
